0f8909292896ace4a4d28463c96a07c88159dac7
pranavchess2004 <86563668+pranavchess2004@users.noreply.github.com>
2021-06-27 21:58:58 +0530

    Delete Note.jsx

--- Note.jsx
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-  <h1>This was an amazing bootcamp taken up by Shaurya Sinha We covered everything from Scratch from Scratch including Javascript React.js, HTML.</h1>,
-  document.getElementById('root')
-);
